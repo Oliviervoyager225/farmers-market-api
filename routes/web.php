039d94cd9437c8a9dbd9dd875f523407bf58cc79
Ouraga Olivier-Noel Dakouri <OD233007@sahanalytics.com>
2026-05-03 20:15:15 +0000

    swagger ui integre dans laravel

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Swagger UI
+Route::get('/api/docs', function () {
+    return view('swagger');
+});
+
+Route::get('/api/docs/openapi.yaml', function () {
+    $path = base_path('openapi.yaml');
+    abort_unless(file_exists($path), 404);
+    return response(file_get_contents($path), 200)
+        ->header('Content-Type', 'application/yaml')
+        ->header('Access-Control-Allow-Origin', '*');
+});
